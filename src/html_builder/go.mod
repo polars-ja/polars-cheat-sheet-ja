fb56c3b198a5b7eddac88fda70c18f3dcba35b89
Shun <s.tsuchiya.business@gmail.com>
2024-08-06 02:05:44 +0900

    headerをtemplate engineで埋め込めるようにしました

---
+++ src/html_builder/go.mod
@@ -0,0 +1,3 @@
+module app/html_builder
+
+go 1.22.5
---
+++ src/html_builder/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+func main() {
+	tmpl := template.Must(template.ParseFiles(
+		"source/header.html",
+		"source/template.html",
+	))
+
+	// 出力ファイルを作成
+	f, err := os.Create("target/output.html")
+	if err != nil {
+		log.Fatalf("ファイル作成に失敗しました: %v", err)
+	}
+	defer f.Close()
+
+	// テンプレートをファイルに書き出す
+	data := ""
+	err = tmpl.ExecuteTemplate(f, "template.html", data)
+	if err != nil {
+		log.Fatalf("テンプレートの実行に失敗しました: %v", err)
+	}
+
+	log.Println("HTMLファイルが作成されました: target/output.html")
+}
